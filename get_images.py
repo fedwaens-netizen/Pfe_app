1b0b6d04a2ccf1959702d6fe1cbd4b266cc30de6
Ahmed-KM-sudo <kassmimohamed93@gmail.com>
2026-06-18 19:53:12 +0100

    Clean up root directory and move scripts to scripts/

--- get_images.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-from sqlalchemy import create_engine
-engine = create_engine('postgresql://postgres:Kxsd2882@localhost:5432/touristes_db')
-df = pd.read_sql("""
-SELECT d.name, d.image_url
-FROM destination d
-WHERE d.name IN ('Chefchaouen', 'Marrakech', 'Merzouga', 'Essaouira', 'Cascades d''Ouzoud', 'Fes', 'Agadir', 'Ifrane')
-""", engine)
-print(df.to_string())
--- update_admin.py
+++
@@ -1,6 +0,0 @@
-from db.database import engine
-from sqlalchemy import text
-with engine.connect() as con:
-    con.execute(text("UPDATE users SET is_admin = true WHERE username = 'admin'"))
-    con.commit()
-print("Admin updated")
